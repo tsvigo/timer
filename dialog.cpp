66cf20924ae40bbe5ddf70dd4092f2f4a2fcc660
Viktor <11_70@bk.ru>
2024-04-12 20:26:54 +0300

    Описание коммита

---
+++ dialog.cpp
@@ -0,0 +1,48 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+/////////////////////////////////////////////
+/// \brief Dialog::Dialog
+/// \param parent
+#include <QTimer>
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \brief Dialog::Dialog
+/// \param parent
+///
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+//    QTimer *timer = new QTimer(this);
+//connect(timer, SIGNAL(timeout()), this, SLOT(update()));
+//timer->start(10000);
+
+///
+/// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+timer=new QTimer; 
+connect(timer,SIGNAL(timeout()),this,SLOT(do_timer())); 
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+
+void Dialog::on_pushButton_clicked()
+{
+        if (!timer->isActive())
+        timer->start(50);
+    else
+        timer->stop();
+}
+
+
+
+void Dialog::do_timer()
+{
+    ui->pushButton->move(QPoint( ui->pushButton->pos().x()+10, ui->pushButton->pos().y()));
+}
